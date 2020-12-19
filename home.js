6e28a5a348df448095cb555c0f3e6c596c35fbf0
evana27perveen <evana27perveen2@gmail.com>
2020-12-19 19:49:28 +0600

    First Commit

---
+++ home.js
@@ -0,0 +1,30 @@
+
+
+a=0;
+let my_num = Math.floor(Math.random() * 10) + 1;
+document.getElementById("btn").addEventListener("click",
+function on_submit()
+{
+    console.log("Random: ", my_num);
+
+let num = document.getElementById("number").value;
+
+if(num == my_num){
+    document.getElementById("result").style="color:green;";
+    document.getElementById("title").style="display:none;";
+    document.getElementById("form").style="display:none;";
+    document.getElementById("result").innerHTML ="<h1>!!! YOU Won !!!</h1>";
+}
+else if(num != my_num && a==2){
+    document.getElementById("result").style="color:red;";
+    document.getElementById("title").style="display:none;";
+    document.getElementById("form").style="display:none;";
+    document.getElementById("result").innerHTML ="<h1>!!! Failed !!!</h1>";
+}
+else{
+    a+=1;
+    document.getElementById("result").innerHTML ="<h3>!!! Try Again !!!</h3>";
+}
+}
+
+)
